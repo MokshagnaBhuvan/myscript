e273a96e58d69d46b8e46dede6bffb0f65808c60
Mokshagna Bhuvan Chowdary Pudota <119480971+MokshagnaBhuvan@users.noreply.github.com>
2024-07-28 21:10:13 +0530

    Add files via upload

---
+++ temp1.js
@@ -0,0 +1,62 @@
+// let name = "Mokshagna Bhuvan";
+// let username = "White7minato";
+
+// result1 = typeof name;
+
+// result2 = typeof username;
+
+// if (result1 === result2) {
+//     console.log("true");
+// }                           //if-else loop
+// else
+//     console.log("false");
+
+// let age = 18;
+
+// result3 = typeof age;
+
+// result4 = (result1 === result3) ? "true" : "false";  //tenary operator
+// console.log(result4);
+
+
+// let day = "Monday";
+
+// switch (day.toLowerCase()) {
+//     case 'monday':
+//     case 'tuesday':
+//     case "wednesday":
+//         console.log("No lab today");
+//         break;
+//     case 'thursday':
+//         console.log("Yes we do hav lab today");
+//         break;
+//     case 'friday':
+//         console.log("No lab today");
+//         break;
+//     default:
+//         console.log("Holiday");
+// }
+
+// console.log(`SO me ${name} with the username ${username}`); //template literal
+
+
+// let i = 4;
+// while (i > 3 && i <= 5) {
+//     console.log("yes it is");
+//     i++;
+// }
+
+
+// for (let a = 1; a <= 100; a++) {
+//     if (a % 3 === 0) {
+//         console.log(a, "Divisible by three");
+//     }
+// }
+
+// let b = 123456;
+// let rem = 0;
+// while (b !== 0) {
+//     rem = b % 10;
+//     console.log(rem);
+//     b = parseInt(b / 10);
+// }
