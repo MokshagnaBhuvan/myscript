9d1ae069880f4aa6dc768c6c8c26624be264bd58
Mokshagna Bhuvan Chowdary Pudota <pmbc2005@outlook.com>
2024-08-30 18:25:23 +0530

    update

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
-public class Main {
+ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the name: ");
-        String name = sc.nextLine();
-        System.out.println("Name of the user is: " + name);
+
+        // String x;
+        // System.out.println("Enter the name of the student: ");
+        // x = sc.nextLine();
+        // System.out.println("Hey " + x);
+
+        double num1 = 11.11;
+        double num2 = (int)num1;
+        System.out.println(num2);
     }
 }
---
+++ java/src/currencyConv.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class currencyConv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-------------------Currency Convertor-------------------");
+        System.out.println("Welcome to the currency coverter application!");
+        System.out.println(
+                "Here we can convert following currencies: \n 1. USD to INR \n 2. INR to USD \n 3. JPY to INR \n 4. INR to JPY \n 5. USD to JPY \n 6. JPY to USD");
+        int choice;
+        System.out.println("Enter the choice of conversion: ");
+        choice = sc.nextInt();
+        String currency = "";
+        switch (choice) {
+            case 1:
+            case 5:
+                currency = "USD";
+                break;
+            case 2:
+            case 4:
+                currency = "INR";
+                break;
+            case 3:
+            case 6:
+                currency = "JPY";
+                break;
+            default:
+                break;
+        }
+        System.out.println("Thanks for choosing us for your conversion needs!");
+        System.out.println("Please enter the amount you need to convert: ");
+        double amount = sc.nextDouble();
+        System.out.println(
+                "For the convertion of " + amount + " " + currency + " we will charge some amount as service charge. ");
+
+        switch (choice) {
+            case 1:
+                System.out.println("The amount you get after converting is " + amount * (82 + 0.2 * amount));
+                break;
+            case 2:
+                System.out.println("The amount you get after converting is " + amount / (0.012 + 0.2 * amount));
+                break;
+            case 3:
+                System.out.println("The amount you get after converting is " + amount / (0.58 + 0.2 * amount));
+                break;
+            case 4:
+                System.out.println("The amount you get after converting is " + amount * (1.72 + 0.2 * amount));
+                break;
+            case 5:
+                System.out.println("The amount you get after converting is " + amount / (144.31 + 0.2 * amount));
+                break;
+            case 6:
+                System.out.println("The amount you get after converting is " + amount * (0.0069 + 0.2 * amount));
+                break;
+        }
+
+    }
+}
